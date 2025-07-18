92cacac2dfdc6254d7a3fada21e2af778bdd3d64
jishnum007 <jishnu@globalsurf.ae>
2025-07-18 11:59:06 +0530

    menu

--- app/components/home/sections/SectionOne.tsx
+++ app/components/home/sections/SectionOne.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 const SectionOne = () => {
   return (
-       <section id="section1" className="h-screen flex items-center justify-center bg-blue-100">
-      <div className="container">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Our Site</h1>
-        <p className="text-lg text-gray-700">This is the hero section with a brief introduction.</p>
+       <section id="section1" className="h-screen flex items-center justify-center  bg-cover bg-center heroafter"
+       style={{
+        backgroundImage: "url('../assets/images/hero-bg1.jpg')", 
+       }}>
+          <div className="absolute inset-0 bg-[#0000008C]"></div>
+      <div className="  relative z-50">
+        <h1 className="text-4xl font-bold mb-4">Trusted Legacy of Engineering Excellence</h1> 
       </div>
     </section>
   )
 }
 
-export default SectionOne
+export default SectionOne 
--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -3,9 +3,11 @@
 import React, { useEffect, useState } from "react";
 
 const sections = [
-  { id: "section1", label: "Home" },
-  { id: "section2", label: "About" },
-  { id: "section3", label: "Contact" },
+  { id: "section1", label: "SP GROUP" },
+  { id: "section2", label: "SPINT" },
+  { id: "section3", label: "SERVICES" },
+  { id: "section4", label: "PROJECTS" },
+  { id: "section5", label: "PEOPLE" }, 
 ];
 
 const Header: React.FC = () => {
@@ -38,21 +40,48 @@ const Header: React.FC = () => {
   }, []);
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow z-50">
-      <nav className="flex justify-center space-x-8 p-4">
+    <>
+    <div className="absolute top-4 right-4 z-50">
+       <div className="flex items-center justify-center bg-black rounded-full p-2 w-11 h-11">
+       <img src="../assets/images/search.svg" alt="Logo" width={18} height={18} />
+       </div>
+      </div>
+    <header className="fixed top-0 left-0 z-50   ">
+      
+    <div className="flex ">
+    <div className="flex w-[150px]">
+    <nav className="flex flex-col ali justify-center   gap-4">
         {sections.map((section) => (
           <a
             key={section.id}
             href={`#${section.id}`}
-            className={`font-medium transition-colors ${
-              activeSection === section.id ? "text-blue-500" : "hover:text-blue-500"
+            className={` transition-colors font-[300] text-white ${
+              activeSection === section.id ? "text-white font-[700] activebfr  " : "nonactivebfr hover:text-white hover:font-[700] "
             }`}
           >
             {section.label}
           </a>
         ))}
-      </nav>
+    </nav>
+    </div>
+     <div className="bg-primary shadow ml-8 my-12 w-[133px] h-[calc(100vh-100px)]">
+     <div className="flex flex-col justify-between h-full py-10">
+     <div className="flex justify-center items-center ">
+      <img src="../assets/images/logo.svg" alt="Logo" width={90} height={55} />
+      </div> 
+      <div className="flex justify-center items-center ">
+      <img src="../assets/images/menu-crbs.svg" alt="Logo" width={31} height={24} />
+      </div>
+      <div className="flex flex-col gap-3 justify-center items-center border-t border-[#ffffff20] ">
+       <p className="text-white font-[300] text-[13px] leading-[25px] pt-3 ">SCROLL DOWN</p>
+       <img src="../assets/images/arrowcircle.svg" alt="Logo" width={87} height={87} />
+      </div>
+     </div>
+     </div>
+    </div>
+     
     </header>
+    </>
   );
 };
 
